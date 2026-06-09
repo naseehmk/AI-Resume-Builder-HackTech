8b423130b5dd75d6ef3619cdaf2e481802155375
Naseeh M <naseehm134@gmail.com>
2026-06-09 15:18:00 +0530

    Update gemini.js

--- js/gemini.js
+++ js/gemini.js
@@ -1,5 +1,5 @@
 const GEMINI_API_KEY = 'AQ.Ab8RN6JjieJhSR2G3mEJfWaZ_Iq4r2lp_92smyD59Nw-CmCfhA';
-const GEMINI_API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${GEMINI_API_KEY}`;
+const GEMINI_API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${GEMINI_API_KEY}`;
 
 async function callGemini(prompt) {
   try {
